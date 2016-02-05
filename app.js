c6e8db22cb062e56ce0cfc40ea582a1a925296e2
Michael Villander <nousvillander@gmail.com>
2016-02-05 00:02:54 -0200

    Adicionando a feature usuarios online e pagina acessada (real time)

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var wine = require('./routes/wines');
 var bodyParser = require('body-parser');
 
@@ -17,6 +19,37 @@ app.post('/wines', wine.addWine);
 // app.delete('/wines/:id', wine.deleteWine);
 
 
-app.listen(3000, function () {
+io.on('connection', function(socket) {
+
+  /*socket.on('message', function (message) {
+    console.log("Got message: " + message);
+    io.sockets.emit('pageview', { 'url': message });
+  });
+
+  socket.emit('news', { hello: 'world carai' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });*/
+
+  socket.on('message', function (message) {
+    console.log("Got message: " + message);
+    var clientIp =  socket.client.request.headers['x-forwarded-for'] || socket.client.conn.remoteAddress || socket.conn.remoteAddress || socket.request.connection.remoteAddress;
+    var timeStamp = new Date();
+    var url = message;
+    var numberClients =  io.engine.clientsCount;
+    io.sockets.emit('clients', {'connections': numberClients});
+    io.sockets.emit('pageview', { 'connections': numberClients, 'ip': '***.***.***.' + clientIp.substring(clientIp.lastIndexOf('.') + 1), 'url': url, 'timestamp': new Date()});
+  });
+
+  socket.on('disconnect', function () {
+    console.log("Socket disconnected 4");
+    var numberClients =  io.engine.clientsCount;
+    io.sockets.emit('clients', {'connections': numberClients});
+  });
+
+});
+
+
+server.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
