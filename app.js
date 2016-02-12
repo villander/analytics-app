07687f0d8de478a9e19f4656bf112bec9c9eef5f
Michael Villander <nousvillander@gmail.com>
2016-02-12 01:24:38 -0200

    Add login with passport

--- app.js
+++ app.js
@@ -5,10 +5,37 @@ var io = require('socket.io')(server);
 var wine = require('./routes/wines');
 var bodyParser = require('body-parser');
 
+
+var passport = require('passport');
+var flash    = require('connect-flash');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
+
 // novos middlewares
+app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(require('method-override')());
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({
+secret: 'ilovescotchscotchyscotchscotch',
+resave: true,
+saveUninitialized: true
+}));// session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// uncomment this line
+require('./config/passport')(passport); // pass passport for configuration
+
 
 app.use(express.static('./public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
---
+++ config/auth.js
@@ -0,0 +1,22 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '542287709267996', // your App ID
+        'clientSecret'  : 'bb57d00feb3d66657c352923a4a7a109', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
