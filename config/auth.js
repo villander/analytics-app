0afaa60128d762fd139dd30b2cfcede3cef36565
Michael Villander <nousvillander@gmail.com>
2016-02-15 01:28:42 -0200

    Replaces login email/password to auth facebook

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,8 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '542287709267996', // your App ID
         'clientSecret'  : 'bb57d00feb3d66657c352923a4a7a109', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
+        'profileFields' : ['id', 'emails', 'first_name', 'last_name', 'gender']
     },
 
     'twitterAuth' : {
