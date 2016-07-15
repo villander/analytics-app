c7211f60adfdb4fae5f12bf9594f27d797b21147
Michael Villander <nousvillander@gmail.com>
2016-07-15 11:39:23 -0300

    Fix dynamic port to server

--- app.js
+++ app.js
@@ -83,6 +83,6 @@ io.on('connection', function(socket) {
 });
 
 
-server.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+server.listen(process.env.PORT || 3000, function(){
+  console.log('listening on');
 });
