4a350be760427190a05e041375d7f2f2b633a263
Michael Villander <nousvillander@gmail.com>
2016-07-15 19:36:07 -0300

    Fix indenting

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,9 @@
 // app/routes.js
 
-module.exports = function(app, passport) {
+module.exports = function (app, passport) {
 
     // route for home page
-    app.get('/', function(req, res) {
+    app.get('/', function (req, res) {
         res.render('index.ejs'); // load the index.ejs file
     });
 
@@ -13,47 +13,47 @@ module.exports = function(app, passport) {
     // route for processing the signup form
 
     // route for showing the profile page
-    app.get('/profile', isLoggedIn, function(req, res) {
+    app.get('/profile', isLoggedIn, function (req, res) {
         app.locals.user = req.user;
         res.render('profile.ejs', {
-            user : req.user // get the user out of session and pass to template
+            user: req.user // get the user out of session and pass to template
         });
     });
 
-    app.get('/site', isLoggedIn, function(req, res) {
+    app.get('/site', isLoggedIn, function (req, res) {
         res.render('site.ejs', {
             user: req.user // get the user out of session and pass to template
         });
     });
 
-    app.get('/site/blog', isLoggedIn, function(req, res) {
+    app.get('/site/blog', isLoggedIn, function (req, res) {
         res.render('blog.ejs', {
             user: req.user // get the user out of session and pass to template
         });
     });
 
-    app.get('/site/about', isLoggedIn, function(req, res) {
+    app.get('/site/about', isLoggedIn, function (req, res) {
         res.render('about.ejs', {
             user: req.user // get the user out of session and pass to template
         });
     });
 
-    app.get('/site/contact', isLoggedIn, function(req, res) {
+    app.get('/site/contact', isLoggedIn, function (req, res) {
         res.render('contact.ejs', {
             user: req.user // get the user out of session and pass to template
         });
     });
 
-    app.get('/site/login', isLoggedIn, function(req, res) {
+    app.get('/site/login', isLoggedIn, function (req, res) {
         res.render('profile.ejs', {
             user: req.user // get the user out of session and pass to template
         });
     });
 
-    app.get('/dashboards', isLoggedIn, function(req, res) {
+    app.get('/dashboards', isLoggedIn, function (req, res) {
         console.log(req.user, 'user session');
         res.render('dashboards.ejs', {
-            user : req.user
+            user: req.user
         });
     })
 
@@ -61,17 +61,17 @@ module.exports = function(app, passport) {
     // FACEBOOK ROUTES =====================
     // =====================================
     // route for facebook authentication and login
-    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
 
     // handle the callback after facebook has authenticated the user
     app.get('/auth/facebook/callback',
         passport.authenticate('facebook', {
-            successRedirect : '/site',
-            failureRedirect : '/'
+            successRedirect: '/site',
+            failureRedirect: '/'
         }));
 
     // route for logging out
-    app.get('/logout', function(req, res) {
+    app.get('/logout', function (req, res) {
         req.logout();
         res.redirect('/');
     });
@@ -80,12 +80,10 @@ module.exports = function(app, passport) {
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
-
     // if user is authenticated in the session, carry on
     if (req.isAuthenticated()) {
         return next();
     }
-
     // if they aren't redirect them to the home page
     res.redirect('/');
 }
